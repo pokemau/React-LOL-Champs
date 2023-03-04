aff790287dcd91c8b49a14f138ad4f1d1aec886e
pokemau <rentillosa90@gmail.com>
2023-03-04 08:13:35 +0800

    test

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,51 @@
-import JsonData from './assets/LOL_Info/champ_info/championFull.json'
+import JsonData from "./assets/LOL_Info/champ_info/championFull.json";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 //components
-import Champ from './Champ';
-import ChampInfo from './ChampInfo';
+import Champ from "./Champ";
+import ChampInfo from "./ChampInfo";
+
+const API_URL =
+  "http://ddragon.leagueoflegends.com/cdn/13.4.1/data/en_US/champion.json";
 
 const Home = () => {
-  const champData = JsonData.data;
+  async function getChampData() {
+    const res = await fetch(API_URL);
+    const champData = await res.json();
 
-  //current champion clicked
-  const [indivData, setIndivData] = useState();
+    return champData;
+  }
+
+  async function showData() {
+    const champData = await getChampData();
+
+    console.log(champData);
+  }
 
   return (
-    <div className="body-container">
-      <div className="left-container">
-        <Champ champData={champData} setIndivData={setIndivData}/>
-      </div>
-
-      <div className="right-container">
-        {indivData && <ChampInfo indivData={indivData}/>}
-      </div>
+    <div>
+      <button onClick={showData}>CLICK</button>
+      <h1>hello</h1>
     </div>
   );
+
+  // const champData = JsonData.data;
+
+  // //current champion clicked
+  // const [indivData, setIndivData] = useState();
+
+  // return (
+  //   <div className="body-container">
+  //     <div className="left-container">
+  //       <Champ champData={champData} setIndivData={setIndivData}/>
+  //     </div>
+
+  //     <div className="right-container">
+  //       {indivData && <ChampInfo indivData={indivData}/>}
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Home;
